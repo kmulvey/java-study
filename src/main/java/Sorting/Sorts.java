74d1aada28dc7695176a70b0cb5db3f3d64fac0d
Kevin Mulvey <kmulvey@linux.com>
2015-10-07 10:25:36 -0400

    merge sort

--- src/main/java/Sorting/Sorts.java
+++ src/main/java/Sorting/Sorts.java
@@ -14,9 +14,32 @@ public class Sorts {
 		}
 		return arr;
 	}
-	
-	public static int[] mergeSort(int[] arr) {
-		
+
+	public static int[] mergeSort(int[] arr, int lo, int hi) {
+		int low = lo, high = hi;
+		if (low >= high)
+			return null;
+
+		int middle = (low + high) / 2;
+		mergeSort(arr, low, middle);
+		mergeSort(arr, middle + 1, high);
+
+		int endLow = middle, startHigh = middle + 1;
+
+		while ((low <= endLow) && (startHigh <= high)) {
+			if (arr[low] < arr[startHigh]) {
+				low++;
+			} else {
+				int temp = arr[startHigh];
+				for (int i = startHigh - 1; i >= low; i--) {
+					arr[i + 1] = arr[i];
+				}
+				arr[low] = temp;
+				low++;
+				endLow++;
+				startHigh++;
+			}
+		}
 		return arr;
 	}
 }
--- src/test/java/Sorting/SortsTest.java
+++ src/test/java/Sorting/SortsTest.java
@@ -4,11 +4,16 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class SortsTest {
-	private int[] unsorted = {11,1,4,76,2,6,8,3,7,36,56,34,87,32,5};
-	private int[] sorted = {1,2,3,4,5,6,7,8,11,32,34,36,56,76,87};
-	
-  @Test
-  public void insertionSort() {
-  	Assert.assertArrayEquals(sorted, Sorts.insertionSort(unsorted));
-  }
+	private int[] unsorted = { 11, 1, 4, 76, 2, 6, 8, 3, 7, 36, 56, 34, 87, 32, 5 };
+	private int[] sorted = { 1, 2, 3, 4, 5, 6, 7, 8, 11, 32, 34, 36, 56, 76, 87 };
+
+	@Test
+	public void insertionSort() {
+		Assert.assertArrayEquals(sorted, Sorts.insertionSort(unsorted));
+	}
+
+	@Test
+	public void mergeSort() {
+		Assert.assertArrayEquals(sorted, Sorts.mergeSort(unsorted, 0, unsorted.length - 1));
+	}
 }
