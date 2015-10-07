4f9dd0c191a33b61303ac0ff7be13cb26f86a3d6
Kevin Mulvey <kmulvey@linux.com>
2015-10-07 19:32:11 -0400

    start graphs

---
+++ src/main/java/Graphs/Vertex.java
@@ -0,0 +1,49 @@
+package Graphs;
+
+public class Vertex {
+	final private String id;
+	final private String name;
+
+	public Vertex(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		return prime + ((id == null) ? 0 : id.hashCode());
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		
+		Vertex other = (Vertex) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		
+		return true;
+	}
+	
+	@Override
+	public String toString(){
+		return name;
+	}
+}
