fb9f08e51c67533c35fc677c21b8a2b163a47f04
Kevin Mulvey <kmulvey@linux.com>
2015-10-08 07:47:42 -0400

    test primes

---
+++ src/test/java/Math/PrimesTest.java
@@ -0,0 +1,21 @@
+package Math;
+
+import java.util.ArrayList;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+public class PrimesTest {
+	@Test
+	public void testPrimes() {
+		ArrayList<Integer> expected = new ArrayList<Integer>();
+		expected.add(2);
+		expected.add(3);
+		expected.add(5);
+		expected.add(7);
+		Assert.assertEquals(expected, Primes.getPrimes(10));
+		System.out.println(Primes.getPrimes(1000));
+	}
+}
