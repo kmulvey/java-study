a972873b36cd5e13f0f95dc9d3621b6a8af2cb80
Kevin Mulvey <kmulvey@linux.com>
2015-10-21 12:03:23 -0400

    more readable

--- src/main/java/Bits/LetterCase.java
+++ src/main/java/Bits/LetterCase.java
@@ -4,7 +4,7 @@ public class LetterCase {
 	public static enum Case { UPPER, LOWER};
 	
 	public static Case getCase(char input){
-		if((input & 0x20) == 32) return Case.LOWER;
+		if((input & 0x20) == 0x20) return Case.LOWER;
 		return Case.UPPER;
 	}
 }
