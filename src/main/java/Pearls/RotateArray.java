b5c9e18a052991353d1a29b43c6ae1bb07e58a7f
Kevin Mulvey <kmulvey@linux.com>
2015-10-22 09:09:45 -0400

    rotate right

--- src/main/java/Pearls/RotateArray.java
+++ src/main/java/Pearls/RotateArray.java
@@ -4,16 +4,35 @@ public class RotateArray<T> {
 	public RotateArray() {
 
 	}
+
 	// Rotate array left while using minimal memory
-	// 
+	// Given: an array and a number of position to shift
+	// Returns: an array shifted by the number of elements specified
 	// Time complexity: f(n) ∈ O(n*numPositions)
 	// Space complexity: f(n) ∈ O(1)
-	public T[] rotate(T[] array, int numPositions) {
+	public T[] rotateLeft(T[] array, int numPositions) {
 		for (int k = 0; k < numPositions; k++) {
-			T temp = array[0];
+			T firstEle = array[0];
 			int i;
 			for (i = 0; i < array.length - 1; i++) {
 				array[i] = array[i + 1];
+			}
+			array[i] = firstEle;
+		}
+		return array;
+	}
+
+	// Rotate array right while using minimal memory
+	// Given: an array and a number of position to shift
+	// Returns: an array shifted by the number of elements specified
+	// Time complexity: f(n) ∈ O(n*numPositions)
+	// Space complexity: f(n) ∈ O(1)
+	public T[] rotateRight(T[] array, int numPositions) {
+		for (int k = 0; k < numPositions; k++) {
+			T temp = array[array.length - 1];
+			int i;
+			for (i = array.length - 1; i > 0; i--) {
+				array[i] = array[i - 1];
 			}
 			array[i] = temp;
 		}
--- src/test/java/Pearls/RotateArrayTest.java
+++ src/test/java/Pearls/RotateArrayTest.java
@@ -1,14 +1,28 @@
 package Pearls;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class RotateArrayTest {
+	private RotateArray<Integer> ra;
+
+	@Before
+	public void setUp() {
+		ra = new RotateArray<Integer>();
+	}
+
 	@Test
-	public void findTest() {
+	public void rotateLeftTest() {
 		Integer[] nums = { 1, 2, 3, 4, 5, 6, 7 };
 		Integer[] expected = { 3, 4, 5, 6, 7, 1, 2 };
-		RotateArray<Integer> ra = new RotateArray<Integer>();
-		Assert.assertArrayEquals(expected, ra.rotate(nums, 2));
+		Assert.assertArrayEquals(expected, ra.rotateLeft(nums, 2));
+	}
+
+	@Test
+	public void rotateRightTest() {
+		Integer[] nums = { 1, 2, 3, 4, 5, 6, 7 };
+		Integer[] expected = { 6, 7, 1, 2, 3, 4, 5 };
+		Assert.assertArrayEquals(expected, ra.rotateRight(nums, 2));
 	}
 }
