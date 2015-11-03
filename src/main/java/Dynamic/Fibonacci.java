fe7160f8012d4f7d048d21d54782632b22f6b2cc
Kevin Mulvey <kmulvey@linux.com>
2015-11-03 16:31:12 -0500

    fast fib

--- src/main/java/Dynamic/Fibonacci.java
+++ src/main/java/Dynamic/Fibonacci.java
@@ -28,4 +28,31 @@ public class Fibonacci {
 		cache.put(n, result);
 		return (result);
 	}
+
+	public int fast(int n) {
+		int i;
+		int[] cache = new int[n + 1];
+		cache[0] = 0;
+		cache[1] = 1;
+		for (i = 2; i <= n; i++) {
+			cache[i] = cache[i - 1] + cache[i - 2];
+		}
+		return cache[n];
+	}
+
+	public int fastest(int n) {
+		if (n == 0)
+			return n;
+
+		int temp = 0, minusOne = 1, minusTwo = 0;
+
+		for (int i = 2; i < n; i++) {
+			temp = minusOne + minusTwo;
+			minusTwo = minusOne;
+			minusOne = temp;
+
+		}
+		return minusOne + minusTwo;
+	}
+
 }
--- src/test/java/Dynamic/FibonacciTest.java
+++ src/test/java/Dynamic/FibonacciTest.java
@@ -6,16 +6,30 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class FibonacciTest {
+	private final int N = 45;
+	private final int EXPECTED = 1134903170 ;
+	
 	@Test
 	public void testSlow() {
 		Fibonacci f = new Fibonacci();
-		Assert.assertEquals(1134903170, f.slow(45));
+		Assert.assertEquals(EXPECTED, f.slow(N));
 	}
 
 	@Test
 	public void testMedium() {
 		Fibonacci f = new Fibonacci();
-		Assert.assertEquals(1134903170, f.medium(45, new Hashtable<Integer, Integer>()));
+		Assert.assertEquals(EXPECTED, f.medium(N, new Hashtable<Integer, Integer>()));
 	}
 
+	@Test
+	public void testFast() {
+		Fibonacci f = new Fibonacci();
+		Assert.assertEquals(EXPECTED, f.fast(N));
+	}
+
+	@Test
+	public void testFastest() {
+		Fibonacci f = new Fibonacci();
+		Assert.assertEquals(EXPECTED, f.fastest(N));
+	}
 }
