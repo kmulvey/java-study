86fb9cdc4b0f564fcc3bf57ba878212a3477d683
Kevin Mulvey <kmulvey@linux.com>
2015-11-09 15:55:47 -0500

    names

--- src/main/java/Strings/Compare.java
+++ src/main/java/Strings/Compare.java
@@ -8,19 +8,19 @@ public class Compare {
 	public Compare() {
 	};
 
-	public int recurseEditDist(String s, String t, int sEnd, int tEnd) {
+	public int recurseEditDist(String s, String t, int lastSCharacter, int lastTCharacter) {
 		int k;
 		int[] opt = new int[3];
 		int lowestCost;
 
-		if (sEnd == 0)
-			return (tEnd * 1);
-		if (tEnd == 0)
-			return (sEnd * 1);
+		if (lastSCharacter == 0)
+			return (lastTCharacter * 1);
+		if (lastTCharacter == 0)
+			return (lastSCharacter * 1);
 
-		opt[MATCH] = recurseEditDist(s, t, sEnd - 1, tEnd - 1) + match(s.charAt(sEnd), t.charAt(tEnd));
-		opt[INSERT] = recurseEditDist(s, t, sEnd, tEnd - 1) + 1;
-		opt[DELETE] = recurseEditDist(s, t, sEnd - 1, tEnd) + 1;
+		opt[MATCH] = recurseEditDist(s, t, lastSCharacter - 1, lastTCharacter - 1) + match(s.charAt(lastSCharacter), t.charAt(lastTCharacter));
+		opt[INSERT] = recurseEditDist(s, t, lastSCharacter, lastTCharacter - 1) + 1;
+		opt[DELETE] = recurseEditDist(s, t, lastSCharacter - 1, lastTCharacter) + 1;
 
 		lowestCost = opt[MATCH];
 		for (k = INSERT; k <= DELETE; k++) {
