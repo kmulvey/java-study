1f30e768b9438b81a1836b9812c41d90fa136255
Kevin Mulvey <kmulvey@linux.com>
2015-11-09 14:53:15 -0500

    recursive edit dist

--- src/main/java/Strings/Compare.java
+++ src/main/java/Strings/Compare.java
@@ -4,29 +4,36 @@ public class Compare {
 	private final int MATCH = 0;
 	private final int INSERT = 1;
 	private final int DELETE = 2;
-	
-	public Compare(){};
-	
-	public int recurseEditDist(String s, String t, int i, int j){
+
+	public Compare() {
+	};
+
+	public int recurseEditDist(String s, String t, int sEnd, int tEnd) {
 		int k;
 		int[] opt = new int[3];
 		int lowestCost;
-		
-		if(i==0) return (j*1);
-		if(j==0) return (i*1);
-		
-		opt[MATCH] = recurseEditDist(s, t, i-1, j-1) + match(s.charAt(i), t.charAt(j));
-		opt[INSERT] = recurseEditDist(s, t, i, j-1) + 1;
-		opt[DELETE] = recurseEditDist(s, t, i-1, j) + 1;
-		
-		lowestCost  = opt[MATCH];
-		for(k=INSERT; k<=DELETE; k++){
-			if(opt[k] < lowestCost) lowestCost = opt[k];
+
+		if (sEnd == 0)
+			return (tEnd * 1);
+		if (tEnd == 0)
+			return (sEnd * 1);
+
+		opt[MATCH] = recurseEditDist(s, t, sEnd - 1, tEnd - 1) + match(s.charAt(sEnd), t.charAt(tEnd));
+		opt[INSERT] = recurseEditDist(s, t, sEnd, tEnd - 1) + 1;
+		opt[DELETE] = recurseEditDist(s, t, sEnd - 1, tEnd) + 1;
+
+		lowestCost = opt[MATCH];
+		for (k = INSERT; k <= DELETE; k++) {
+			if (opt[k] < lowestCost)
+				lowestCost = opt[k];
 		}
 		return lowestCost;
 	}
-	private int match(char c, char d){
-		if(c==d) return 0;
-		else return 1;
+
+	private int match(char c, char d) {
+		if (c == d)
+			return 0;
+		else
+			return 1;
 	}
 }
--- src/test/java/Strings/CompareTest.java
+++ src/test/java/Strings/CompareTest.java
@@ -3,12 +3,13 @@ package Strings;
 import org.junit.Assert;
 import org.junit.Test;
 
-
 public class CompareTest {
- @Test
- public void testRecurseEditDist(){
-	 Compare c = new Compare();
-	 Assert.assertEquals(1, c.recurseEditDist("shot", "spot", 3, 3));
-	 Assert.assertEquals(2, c.recurseEditDist("shot", "spit", 3, 3));
- }
+	@Test
+	public void testRecurseEditDist() {
+		Compare c = new Compare();
+		Assert.assertEquals(1, c.recurseEditDist("shot", "spot", 3, 3));
+		Assert.assertEquals(2, c.recurseEditDist("shot", "spit", 3, 3));
+
+		Assert.assertEquals(1, c.recurseEditDist("programmer", "programmers", 9, 10));
+	}
 }
