1ff573eccd7ebd6d63c579842b25fee58926fac7
Kevin Mulvey <kmulvey@linux.com>
2015-11-10 17:24:00 -0500

    partition tests

---
+++ src/test/java/Dynamic/PartitionTest.java
@@ -0,0 +1,14 @@
+package Dynamic;
+
+import org.junit.Test;
+
+public class PartitionTest {
+	@Test
+	public void testEqual(){
+		int[] hundreds = {100,100,100,100,100,100,100,100,100};
+	}
+	@Test
+	public void testIncremental(){
+		int[] hundreds = {100,200,300,400,500,600,700,800,900};
+	}
+}
