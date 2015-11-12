94a124bc76aba908af61b69bcb40e9f02c03635b
Kevin Mulvey <kmulvey@linux.com>
2015-11-12 09:34:08 -0500

    dynamic partition

--- src/main/java/Dynamic/Partition.java
+++ src/main/java/Dynamic/Partition.java
@@ -5,11 +5,10 @@ import java.util.ArrayList;
 import Sorting.Sorts;
 
 public class Partition {
-	public Partition(){
-		
+	public Partition() {
+
 	}
-	
-	
+
 	public ArrayList<ArrayList<Integer>> greedy(int[] values, int K) {
 		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
 
@@ -44,36 +43,76 @@ public class Partition {
 			sum += x[i];
 		return sum;
 	}
-	
-	private int minimumPossibilityLength(int[] books, int n, int k){
-		if(k==1) return sum(books, 0, n);
-		if(n==1) return books[0];
+
+	private int minimumPossibilityLength(int[] books, int n, int k) {
+		if (k == 1)
+			return sum(books, 0, n);
+		if (n == 1)
+			return books[0];
 		int min = Integer.MAX_VALUE;
-		for(int i =0; i<n; i++){
-			int a = minimumPossibilityLength(books, i+1, k-1);
-			int b = sum(books, i+1, n);
+		for (int i = 0; i < n; i++) {
+			int a = minimumPossibilityLength(books, i + 1, k - 1);
+			int b = sum(books, i + 1, n);
 			min = Math.min(min, Math.max(a, b));
 		}
 		return min;
 	}
-	
-	public int[] solve(int[] books, int k){
-		assert k>0;
-		assert books.length >=k;
-		
+
+	public int[] solveRecursive(int[] books, int k) {
+		assert k > 0;
+		assert books.length >= k;
+
 		int min = minimumPossibilityLength(books, books.length, k);
-		int[] dividers = new int[k-1];
-		int sum = 0, d=0;
-		
-		for(int i=0; i<books.length; i++){
-			if(sum + books[i]  > min){
+		int[] dividers = new int[k - 1];
+		int sum = 0, d = 0;
+
+		for (int i = 0; i < books.length; i++) {
+			if (sum + books[i] > min) {
 				dividers[d++] = i;
 				sum = books[i];
-			} else{
+			} else {
 				sum += books[i];
 			}
 		}
 		assert d == dividers.length;
+		return dividers;
+	}
+
+	public int[] solveDynamic(int[] books, int k) {
+		assert k > 0 && books.length >= k;
+
+		final int[] sum = new int[books.length];
+		final int[][] M = new int[books.length + 1][k + 1];
+		final int[][] D = new int[books.length + 1][k + 1];
+		
+		sum[0] = books[0];
+
+		for (int i = 1; i < books.length; i++)
+			sum[i] = sum[i - 1] + books[i];
+
+
+
+		for (int n = 1; n <= books.length; n++)
+			M[n][1] = sum[n - 1];
+		for (int m = 1; m <= k; m++)
+			M[1][m] = books[0];
+
+		for (int n = 2; n <= books.length; n++) {
+			for (int m = 2; m <= k; m++) {
+				M[n][m] = Integer.MAX_VALUE;
+				for (int x = 1; x < n; x++) {
+					final int largest = Math.max(M[x][m - 1], sum[n - 1] - sum[x - 1]);
+
+					if (largest < M[n][m]) {
+						M[n][m] = largest;
+						D[n][m] = x;
+					}
+				}
+			}
+		}
+		int[] dividers = new int[k - 1];
+		for (int m = k, n = books.length; m > 1; m--)
+			n = dividers[m - 2] = D[n][m];
 		return dividers;
 	}
 }
--- src/test/java/Dynamic/PartitionTest.java
+++ src/test/java/Dynamic/PartitionTest.java
@@ -15,17 +15,35 @@ public class PartitionTest {
 	@Test
 	public void oneWorker() {
 		int[] hundreds = { 100, 100, 100, 100, 100, 100, 100, 100, 100 };
-		Assert.assertArrayEquals(new int[] {}, p.solve(hundreds, 1));
+		Assert.assertArrayEquals(new int[] {}, p.solveRecursive(hundreds, 1));
+		Assert.assertArrayEquals(new int[] {}, p.solveDynamic(hundreds, 1));
+	}
+	
+	@Test
+	public void evenLength() {
+		int[] hundreds = { 100, 100, 100, 100, 100, 100, 100, 100, 100 };
+		Assert.assertArrayEquals(new int[] {3,6}, p.solveRecursive(hundreds, 3));
+		Assert.assertArrayEquals(new int[] {3,6}, p.solveDynamic(hundreds, 3));
+	}
+	
+	@Test
+	public void evenWork() {
+		int[] hundreds = { 100, 100, 100, 100, 200, 200, 400};
+		Assert.assertArrayEquals(new int[] {4,6}, p.solveRecursive(hundreds, 3));
+		Assert.assertArrayEquals(new int[] {4,6}, p.solveDynamic(hundreds, 3));
 	}
 
 	@Test
 	public void twoWorkers() {
 		int[] hundreds = { 100, 200 };
-		Assert.assertArrayEquals(new int[] { 1 }, p.solve(hundreds, 2));
+		Assert.assertArrayEquals(new int[] { 1 }, p.solveRecursive(hundreds, 2));
+		Assert.assertArrayEquals(new int[] { 1 }, p.solveDynamic(hundreds, 2));
 	}
 
 	@Test
 	public void testIncremental() {
 		int[] hundreds = { 100, 200, 300, 400, 500, 600, 700, 800, 900 };
+		Assert.assertArrayEquals(new int[] { 5,7 }, p.solveRecursive(hundreds, 3));
+		Assert.assertArrayEquals(new int[] { 5,7 }, p.solveDynamic(hundreds, 3));
 	}
 }
