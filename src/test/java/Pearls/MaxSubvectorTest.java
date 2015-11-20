f3e640697c21d69d6e4a908c2aa03cb4b84a42b0
Kevin Mulvey <kmulvey@linux.com>
2015-11-20 10:43:32 -0500

    moar tests

--- src/test/java/Pearls/MaxSubvectorTest.java
+++ src/test/java/Pearls/MaxSubvectorTest.java
@@ -7,9 +7,33 @@ import org.junit.Test;
 public class MaxSubvectorTest {
 
 	@Test
-	public void findTest() {
+	public void maxTest() {
 		int[] data = { 31, -41, 59, 26, -53, 58, 97, -93, -23, 84 };
 		Assert.assertEquals(MaxSubvector.max(data), 187);
-		
+	}
+	@Test
+	public void singleValTest() {
+		int[] data = { 31};
+		Assert.assertEquals(MaxSubvector.max(data), 31);
+	}
+	@Test
+	public void zeroTest() {
+		int[] data = { 0 };
+		Assert.assertEquals(MaxSubvector.max(data), 0);
+	}
+	@Test
+	public void emptyTest() {
+		int[] data = { };
+		Assert.assertEquals(MaxSubvector.max(data), 0);
+	}
+	@Test
+	public void negativeTest() {
+		int[] data = { -31, -45 };
+		Assert.assertEquals(MaxSubvector.max(data), 0);
+	}
+	@Test
+	public void lonelyTest() {
+		int[] data = { -31, -41, 59 };
+		Assert.assertEquals(MaxSubvector.max(data), 59);
 	}
 }
