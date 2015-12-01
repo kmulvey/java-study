517d43998d30543531795578135c04eb26e98e13
Kevin Mulvey <kmulvey@linux.com>
2015-12-01 09:44:04 -0500

    max sub vector

--- src/main/java/Pearls/MaxSubvector.java
+++ src/main/java/Pearls/MaxSubvector.java
@@ -2,18 +2,18 @@ package Pearls;
 
 public class MaxSubvector {
 
-	public static int max(int[] arr){
-		int maxSoFar=0; // i-1 sum
+	public static int max(int[] arr) {
+		int maxSoFar = 0; // i-1 sum
 		int maxNow = 0; // i sum
-		for(int i=0; i<arr.length; i++){
-			maxNow  = max(maxNow + arr[i], 0);
-			maxSoFar= max(maxSoFar, maxNow);
+		for (int i = 0; i < arr.length; i++) {
+			maxNow = max(maxNow + arr[i], 0); // dont go backwards
+			maxSoFar = max(maxSoFar, maxNow);
 		}
 		return maxSoFar;
 	}
-	
-	private static int max(int x, int y){
-		if(x>y)return x;
-		return y;
+
+	private static int max(int x, int y) {
+		return x > y ? x : y;
 	}
 }
+ 
