d0ae8fcd66410c39f63b5b16c5cef240fa1ed7ef
Kevin Mulvey <kmulvey@linux.com>
2015-12-01 09:45:35 -0500

    use all tests

--- src/test/java/Pearls/MaxSubvectorTest.java
+++ src/test/java/Pearls/MaxSubvectorTest.java
@@ -3,43 +3,47 @@ package Pearls;
 import org.junit.Assert;
 import org.junit.Test;
 
-
 public class MaxSubvectorTest {
 
-//	@Test
-//	public void maxTest() {
-//		int[] data = { 31, -41, 59, 26, -53, 58, 97, -93, -23, 84 };
-//		Assert.assertEquals(MaxSubvector.max(data), 187);
-//	}
-//	@Test
-//	public void singleValTest() {
-//		int[] data = { 31};
-//		Assert.assertEquals(MaxSubvector.max(data), 31);
-//	}
-//	@Test
-//	public void zeroTest() {
-//		int[] data = { 0 };
-//		Assert.assertEquals(MaxSubvector.max(data), 0);
-//	}
-//	@Test
-//	public void emptyTest() {
-//		int[] data = { };
-//		Assert.assertEquals(MaxSubvector.max(data), 0);
-//	}
-//	@Test
-//	public void negativeTest() {
-//		int[] data = { -31, -45 };
-//		Assert.assertEquals(MaxSubvector.max(data), 0);
-//	}
-//	@Test
-//	public void lonelyTest() {
-//		int[] data = { -31, -41, 59 };
-//		Assert.assertEquals(MaxSubvector.max(data), 59);
-//	}
-	
+	@Test
+	public void maxTest() {
+		int[] data = { 31, -41, 59, 26, -53, 58, 97, -93, -23, 84 };
+		Assert.assertEquals(MaxSubvector.max(data), 187);
+	}
+
+	@Test
+	public void singleValTest() {
+		int[] data = { 31 };
+		Assert.assertEquals(MaxSubvector.max(data), 31);
+	}
+
+	@Test
+	public void zeroTest() {
+		int[] data = { 0 };
+		Assert.assertEquals(MaxSubvector.max(data), 0);
+	}
+
+	@Test
+	public void emptyTest() {
+		int[] data = {};
+		Assert.assertEquals(MaxSubvector.max(data), 0);
+	}
+
+	@Test
+	public void negativeTest() {
+		int[] data = { -31, -45 };
+		Assert.assertEquals(MaxSubvector.max(data), 0);
+	}
+
+	@Test
+	public void lonelyTest() {
+		int[] data = { -31, -41, 59 };
+		Assert.assertEquals(MaxSubvector.max(data), 59);
+	}
+
 	@Test
 	public void waveTest() {
-		int[] data = { 0,1, 2,3,4,5,-6,-7, -8,-9, 10, 11, 12, 13, 14 };
+		int[] data = { 0, 1, 2, 3, 4, 5, -6, -7, -8, -9, 10, 11, 12, 13, 14 };
 		Assert.assertEquals(MaxSubvector.max(data), 60);
 	}
 }
