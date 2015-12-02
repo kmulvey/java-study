70e3560008ae73d9c5c9e5b8344a85cb59afce1a
Kevin Mulvey <kmulvey@linux.com>
2015-12-02 10:04:41 -0500

    better tests

--- src/main/java/Pearls/Partition.java
+++ src/main/java/Pearls/Partition.java
@@ -4,33 +4,32 @@ import java.util.Arrays;
 
 public class Partition {
 
-	public static boolean isEven(int[] set){
+	public static boolean isEven(int[] set) {
 		int sum = 0;
-		for(int i=0; i<set.length; i++){
+		for (int i = 0; i < set.length; i++) {
 			sum += set[i];
 		}
-		
-		if(sum%2 != 0) return false;
+
+		if (sum % 2 != 0) return false;
 
 		// empty boolean table of size (⌊K/2⌋ + 1) by (n + 1)
-		boolean[][] table = new boolean[sum/2+1][set.length +1];
-		
+		boolean[][] table = new boolean[sum / 2 + 1][set.length + 1];
+
 		// initialize top row (P(0,x)) of P to True
 		Arrays.fill(table[0], true);
-		
+
 		// initialize leftmost column (P(x, 0)) of P, except for P(0, 0) to False
-		for(int i=1; i<table.length; i++){
+		for (int i = 1; i < table.length; i++) {
 			table[i][0] = false;
 		}
-		
-		for(int i =1; i<=sum/2; i++){
-			for(int j = 1; j<=set.length; j++){
-				table[i][j] = table[i][j-1];
-				if(i>=set[j-1])
-					table[i][j] = table[i][j] || table[i-set[j-1]][j-1];
+
+		for (int i = 1; i <= sum / 2; i++) {
+			for (int j = 1; j <= set.length; j++) {
+				table[i][j] = table[i][j - 1];
+				if (i >= set[j - 1]) table[i][j] = table[i][j] || table[i - set[j - 1]][j - 1];
 			}
 		}
-		
-		return table[sum/2][set.length];
+
+		return table[sum / 2][set.length];
 	}
 }
--- src/test/java/Pearls/PartitionTest.java
+++ src/test/java/Pearls/PartitionTest.java
@@ -3,18 +3,35 @@ package Pearls;
 import org.junit.Assert;
 import org.junit.Test;
 
-
 public class PartitionTest {
 	@Test
-	public void simpleTest(){
-		int[] set = {1,1,1,2};
+	public void oneTest() {
+		int[] set = { 1, 1, 1, 2 };
 		Assert.assertEquals(false, Partition.isEven(set));
 	}
+
 	@Test
-	public void simplasdeTest(){
-		int[] set = {3,1,1,2,2,1};
+	public void twoTest() {
+		int[] set = { 3, 1, 1, 2, 2, 1 };
 		Assert.assertEquals(true, Partition.isEven(set));
 	}
 	
+	@Test
+	public void threeTest() {
+		int[] set = {4, 5, 6, 7, 8};
+		Assert.assertEquals(true, Partition.isEven(set));
+	}
+	
+	@Test
+	public void fourTest() {
+		int[] set = {1, 5, 11, 5};
+		Assert.assertEquals(true, Partition.isEven(set));
+	}
+	
+	@Test
+	public void fiveTest() {
+		int[] set = {1, 5, 3};
+		Assert.assertEquals(false, Partition.isEven(set));
+	}
 	
 }
