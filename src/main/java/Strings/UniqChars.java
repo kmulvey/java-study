0cbb9061731110967944f0939e8334817160d9c7
Kevin Mulvey <kmulvey@linux.com>
2015-12-02 13:59:41 -0500

    uniq characters using bit vector

--- src/main/java/Strings/UniqChars.java
+++ src/main/java/Strings/UniqChars.java
@@ -1,5 +1,6 @@
 package Strings;
 
+import java.util.BitSet;
 import java.util.HashMap;
 
 public class UniqChars {
@@ -12,13 +13,14 @@ public class UniqChars {
 		return true;
 	}
 
-//	public static boolean hasUniqCharsBit(String input) {
-//		boolean[] alpha = new boolean[26];
-//		
-//		for (int i = 0; i < input.length(); i++) {
-//			if (map.containsKey(input.charAt(i))) return false;
-//			map.put(input.charAt(i), 1);
-//		}
-//		return true;
-//	}
+	// assumes ascii
+	public static boolean hasUniqCharsBit(String input) {
+		BitSet map = new BitSet(128);
+		
+		for (int i = 0; i < input.length(); i++) {
+			if (map.get((int) input.charAt(i))) return false;
+			map.set((int) input.charAt(i));
+		}
+		return true;
+	}
 }
--- src/test/java/Strings/UniqCharsTest.java
+++ src/test/java/Strings/UniqCharsTest.java
@@ -6,8 +6,16 @@ import org.junit.Test;
 
 public class UniqCharsTest {
 	@Test
-	public void fifteenTest() {
+	public void asciiTest() {
 		Assert.assertEquals(true, UniqChars.hasUniqChars("sdf"));
 		Assert.assertEquals(false, UniqChars.hasUniqChars("sddf"));
+		
+		Assert.assertEquals(true, UniqChars.hasUniqCharsBit("sdf"));
+		Assert.assertEquals(false, UniqChars.hasUniqCharsBit("sddf"));
+	}
+	@Test
+	public void unicodeTest() {
+		Assert.assertEquals(true, UniqChars.hasUniqChars("⟅⟆"));
+		Assert.assertEquals(false, UniqChars.hasUniqChars("⟚⟚bt"));
 	}
 }
